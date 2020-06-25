76a6339a17e59bca5222593aa1008bd718882088
Ibbs620 <58309356+Ibbs620@users.noreply.github.com>
2020-06-25 15:38:48 -0400

    Operations can now be done one after another without pressing the = button. Every time an operator is pressed, it will the display the answer so far. Some code was also cleaned up.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private String text = "0";
     private double num1 = 0;
     private double num2 = 0;
+    private double backup;
     private char op;
     private boolean equalPressed = false;
     private boolean opPressed = false;
@@ -31,6 +32,57 @@ public class MainActivity extends AppCompatActivity {
         showNumber = findViewById(R.id.display);
         showNumber.setText(text);
         dc.setRoundingMode(RoundingMode.HALF_UP);
+    }
+
+    public void operation() {
+        num2 = Double.parseDouble(text);
+        switch (op) {
+            case '+':
+                num1 += num2;
+                break;
+            case '-':
+                num1 -= num2;
+                break;
+            case '/':
+                num1 /= num2;
+                break;
+            case '*':
+                num1 *= num2;
+                break;
+            case '^':
+                num1 = Math.pow(num1, num2);
+                backup = num1;
+                break;
+            default:
+        }
+        num2 = 0;
+        text = format_num(num1);
+        showNumber.setText(text);
+    }
+
+    public String format_num(double num) {
+        String text;
+        if (num == 0) {
+            text = "0";
+        } else if (num >= 10000000 && num < 9.99999999999E11 || num <= -10000000 && num > -9.99999999999E11) {
+            text = dc.format(num);
+            text = dc.format(num);
+            StringBuilder text2 = new StringBuilder();
+            for (int i = 0; i <= text.length() - 1; i++) {
+                if (text.charAt(i) != ',') text2.append(text.charAt(i));
+            }
+            text = text2.toString();
+        } else if (num >= 1.0E100 || num <= -1.0E100 || Math.log10(num) <= -100 || (num > -1 && num < -1.0E-100)) {
+            text = "Overflow";
+        } else {
+            text = Double.toString(num);
+            if (num > 9.99999999999E11) text = String.format("%.7e", num);
+            else if (num < -9.99999999999E11) text = String.format("%.6e", num);
+            else if (Math.log10(num) < -11) text = String.format("%.6e", num);
+            else if (num > -1 && num < -0.00000000001) text = String.format("%.6e", num);
+            else if (num % 1 == 0) text = text.substring(0, text.length() - 2);
+        }
+        return text;
     }
 
     public void zero_pressed(View view) {
@@ -180,9 +232,12 @@ public class MainActivity extends AppCompatActivity {
         }
         text += ".";
         showNumber.setText(text);
+        opPressed = false;
+        equalPressed = false;
     }
 
     public void neg_pressed(View view) {
+        if (opPressed) return;
         if (text.equals("0")) return;
         if (text.charAt(0) != '-') text = "-" + text;
         else {
@@ -193,132 +248,70 @@ public class MainActivity extends AppCompatActivity {
 
     public void equal_pressed(View view) {
         if (opPressed) return;
-        num2 = Double.parseDouble(text);
-        switch (op) {
-            case '+':
-                num1 += num2;
-                break;
-            case '-':
-                num1 -= num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    text = "Error";
-                    showNumber.setText(text);
-                    text = "0";
-                    num2 = 0;
-                    equalPressed = true;
-                }
-                num1 /= num2;
-                break;
-            case '*':
-                int i = 0;
-                while (num1 % 1 != 0 && num2 % 1 != 0) {
-                    i++;
-                    num1 *= 10;
-                    num2 *= 10;
-                }
-                num1 *= num2;
-                i *= 2;
-                while (i > 0) {
-                    num1 /= 10;
-                    i--;
-                }
-                break;
-            case '^':
-                num1 = Math.pow(num1, num2);
-                break;
-            default:
-        }
-        if (num1 > 9.99999999999E+11 || num1 < -9.99999999999E+11) {
-            for (int i = text.length() - 1; i >= 0; i--) {
-                if (text.charAt(i) == 'E') {
-                    text = text.substring(0, 11) + text.substring(i, text.length() - 1);
-                    break;
-                }
-            }
-        } else if (num1 >= 10000000 || num1 <= -10000000) {
-            text = dc.format(num1);
-            StringBuilder text2 = new StringBuilder();
-            for (int i = 0; i <= text.length() - 1; i++) {
-                if (text.charAt(i) != ',') text2.append(text.charAt(i));
-            }
-            text = text2.toString();
-        } else {
-            text = Double.toString(num1);
-            if (num1 % 1 == 0) text = text.substring(0, text.length() - 2);
-        }
-        showNumber.setText(text);
-        text = "0";
-        num2 = 0;
+        operation();
         equalPressed = true;
     }
 
     public void plus_pressed(View view) {
-        op = '+';
         if (opPressed) return;
-        if (num1 != 0 && num2 != 0) {
-            num1 += num2;
-            num2 = 0;
-            text = Double.toString(num1);
-            showNumber.setText(text);
-        } else if (num1 == 0) num1 = Double.parseDouble(text);
-        else num2 = Double.parseDouble(text);
+        if (num1 == 0) num1 = Double.parseDouble(text);
+        else {
+            operation();
+        }
+        op = '+';
         text = "";
         opPressed = true;
+        equalPressed = false;
     }
 
     public void minus_pressed(View view) {
-        op = '-';
         if (opPressed) return;
-        if (num1 != 0 && num2 != 0) {
-            num1 -= num2;
-            num2 = 0;
-            text = Double.toString(num1);
-            showNumber.setText(text);
-        } else if (num1 == 0) num1 = Double.parseDouble(text);
-        else num2 = Double.parseDouble(text);
+        if (num1 == 0) num1 = Double.parseDouble(text);
+        else {
+            operation();
+        }
+        op = '-';
         text = "";
         opPressed = true;
+        equalPressed = false;
     }
 
     public void divide_pressed(View view) {
-        op = '/';
         if (opPressed) return;
-        if (num1 != 0 && num2 != 0) {
-            num1 /= num2;
-            num2 = 0;
-            text = Double.toString(num1);
-            showNumber.setText(text);
-        } else if (num1 == 0) num1 = Double.parseDouble(text);
-        else num2 = Double.parseDouble(text);
+        if (num1 == 0) num1 = Double.parseDouble(text);
+        else {
+            operation();
+        }
+        op = '/';
         text = "";
         opPressed = true;
+        equalPressed = false;
     }
 
     public void multiply_pressed(View view) {
-        op = '*';
         if (opPressed) return;
-        if (num1 != 0 && num2 != 0) {
-            num1 *= num2;
-            num2 = 0;
-            text = Double.toString(num1);
-            showNumber.setText(text);
-        } else if (num1 == 0) num1 = Double.parseDouble(text);
-        else num2 = Double.parseDouble(text);
+        if (num1 == 0) num1 = Double.parseDouble(text);
+        else {
+            operation();
+        }
+        op = '*';
         text = "";
         opPressed = true;
+        equalPressed = false;
     }
 
     public void power_pressed(View view) {
-        op = '^';
         if (opPressed) return;
-        if (num1 != 0 && num2 != 0) {
-            Toast.makeText(this, "Please press enter", Toast.LENGTH_SHORT).show();
+        if (equalPressed) {
+            num1 = Double.parseDouble(text);
         } else if (num1 == 0) num1 = Double.parseDouble(text);
-        else num2 = Double.parseDouble(text);
+        else {
+            operation();
+        }
+        op = '^';
         text = "";
         opPressed = true;
+        equalPressed = false;
     }
 
     public void back_pressed(View view) {
